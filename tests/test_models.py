0072e48ecefd4efe72fc915d9d09dba871b3a40c
june <june@ticketplace.net>
2015-12-31 13:59:26 +0900

    Fix test

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,31 +4,10 @@
 import datetime
 import pytest
 
-from ticketplace.models import db, User, Company, Content
+from ticketplace.models import db, Company, Content
 from ticketplace.utils import kst_now
 
 create_user = False
-
-
-@pytest.mark.usefixtures("testapp")
-class TestModels:
-    def test_user_save(self, testapp):
-        """ Test Saving the user model to the database """
-
-        admin = User('admin', 'supersafepassword')
-        db.session.add(admin)
-        db.session.commit()
-
-        user = User.query.filter_by(username="admin").first()
-        assert user is not None
-
-    def test_user_password(self, testapp):
-        """ Test password hashing and checking """
-
-        admin = User('admin', 'supersafepassword')
-
-        assert admin.username == 'admin'
-        assert admin.check_password('supersafepassword')
 
 
 @pytest.mark.usefixtures("testapp")
