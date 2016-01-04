483926176aa6b9d51dbdc88bed60966eae5bfa06
june <june@ticketplace.net>
2016-01-04 11:37:50 +0900

    Waypoint bug fixed

--- ticketplace/static/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function( ){  // waypoint custom offset effect
-   var sticky = new Waypoint.Sticky({
-      element: $('nav.navbar')[0],
-      stuckClass: 'navback navback-fixed-top'
-    })     
-}); 
